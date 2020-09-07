401cc86044e514441ca53e9cb97a03e568cc7dda
ehcho0222 <64142236+ehcho0222@users.noreply.github.com>
2020-09-07 22:04:15 +0900

    Food & Wood

--- scripts/main.js
+++ scripts/main.js
@@ -17,6 +17,18 @@ var curPop = document.querySelector('.curpop');
 curPop.textContent = pop;
 var maxPop = document.querySelector('.maxpop');
 maxPop.textContent = popCap;
+var curFood = document.querySelector('.curfood');
+curFood.textContent = food;
+var maxFood = document.querySelector('.maxfood');
+maxFood.textContent = foodthr;
+var foodIncome = document.querySelector('.foodincome');
+maxFood.textContent = foodps;
+var curWood = document.querySelector('.curwood');
+curWood.textContent = wood;
+var maxWood = document.querySelector('.maxwood');
+maxWood.textContent = woodCap;
+var woodIncome = document.querySelector('.woodincome');
+woodIncome.textContent = woodps;
 function incrementResources() {
 	food += foodps;
 	if (food >= foodthr && pop < popCap) {
@@ -25,6 +37,9 @@ function incrementResources() {
 	}
 	curPop.textContent = pop;
 	maxPop.textContent = popCap;
+	curFood.textContent = food;
+	maxFood.textContent = foodthr;
+	foodIncome.textContent = foodps;
 	if (pop >= popCap) {
 		curPop.style.color = 'red';
 		maxPop.style.color = 'red';
@@ -35,6 +50,16 @@ function incrementResources() {
 	wood += woodps;
 	if (wood > woodCap) {
 		wood = woodCap;
+	}
+	curWood.textContent = wood;
+	maxWood.textContent = woodCap;
+	woodIncome.textContent = woodps;
+	if (wood >= woodCap * 0.95) {
+		curWood.style.color = '#ffe000';
+		maxWood.style.color = '#ffe000';
+	} else {
+		curWood.style.color = '#e0e0e0';
+		maxWood.style.color = '#e0e0e0';
 	}
 	stone += stoneps;
 	if (stone > stoneCap) {
