9b2681b8938b46a8e6e6c7317e3fb305288a49b0
ehcho0222 <64142236+ehcho0222@users.noreply.github.com>
2020-09-07 21:51:23 +0900

    Population

--- scripts/main.js
+++ scripts/main.js
@@ -8,15 +8,29 @@ var stone = 0;
 var stoneps = 1;
 var stoneCap = 100;
 var pop = 1;
+var popCap = 6;
 var science = 0;
 var keepRatio = 0.0;
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Ancient Era';
+var era = document.querySelector('h1');
+era.textContent = 'Ancient Era';
+var curPop = document.querySelector('.curpop');
+curPop.textContent = pop;
+var maxPop = document.querySelector('.maxpop');
+maxPop.textContent = popCap;
 function incrementResources() {
 	food += foodps;
-	if (food >= foodthr) {
+	if (food >= foodthr && pop < popCap) {
 		food *= keepRatio;
 		pop += 1;
+	}
+	curPop.textContent = pop;
+	maxPop.textContent = popCap;
+	if (pop >= popCap) {
+		curPop.style.color = 'red';
+		maxPop.style.color = 'red';
+	} else {
+		curPop.style.color = '#e0e0e0';
+		maxPop.style.color = '#e0e0e0';
 	}
 	wood += woodps;
 	if (wood > woodCap) {
