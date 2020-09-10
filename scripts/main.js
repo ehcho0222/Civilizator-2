91a9094c709ca9f52dc2c5b554e17cd61b3aa7e3
ehcho0222 <64142236+ehcho0222@users.noreply.github.com>
2020-09-10 09:56:13 +0900

    aoeg

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,15 @@
 var food = 10;
-var prod = 0;
-var prodpt = 1;
+var prod = 1;
+var prodpt = 2;
 var pop = 10;
 var popCap = 25;
-var science = 2;
+var science = 1;
+var scipt = 1;
+var culture = 1;
+var culpt = 1;
 var era = 0;
 var eras = ['Ancient Era', 'Classical Era', 'Medieval Era', 'Renaissance Era', 'Industrial Era', 'Modern Era', 'Future'];
+var buildingBonus = [0, 0, 0, 0, 0, 0];
 
 var curEra = document.querySelector('h1');
 era.textContent = 'Ancient Era';
@@ -19,12 +23,20 @@ var curProd = document.querySelector('.curprod');
 curProd.textContent = prod;
 var prodIncome = document.querySelector('.prodincome');
 prodIncome.textContent = prodpt;
+var curScience = document.querySelector('.cursci');
+curScience.textContent = scipt;
 var scienceIncome = document.querySelector('.science');
 scienceIncome.textContent = science;
+var curCulture = document.querySelector('.curcul');
+curCulture.textContent = culture;
+var cultureIncome = document.querySelector('.cpt');
+cultureIncome.textContent = culpt;
 
 function incrementResources() {
 	pop += food / 20;
 	prod += prodpt;
+	science += scipt;
+	culture += culpt;
 }
 
 function displayResources() {
@@ -47,14 +59,25 @@ function displayResources() {
 		curPop.style.color = '#e0e0e0';
 		maxPop.style.color = '#e0e0e0';
 	}
-	curProd.textContent = Math.round(prod);
+	curProd.textContent = Math.round(prod * 10) / 10;
 	prodIncome.textContent = Math.round(prodpt * 10) / 10;
-	scienceIncome.textContent = Math.round(science * 10) / 10;
+	curScience.textContent = Math.round(science * 10) / 10;
+	scienceIncome.textContent = Math.round(scipt * 10) / 10;
+	curCulture.textContent = Math.round(culture * 10) / 10;
+	cultureIncome.textContent = Math.round(culpt * 10) / 10;
 	curEra.textContent = eras[era];
+}
+
+function calcResources() {
+	food = 10 - (pop / 10) + buildingBonus[0];
+	prodpt = 2 + buildingBonus[1];
+	scipt = 1 + buildingBonus[2];
+	culpt = 1 + buildingBonus[3];
 }
 
 function nextTurn() {
 	incrementResources();
+	calcResources();
 	displayResources();
 }
 
